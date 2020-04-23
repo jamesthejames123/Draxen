c09ef8951f77749f6eb50a4318525e9e0ab2b169
jamesthejames123 <jamesthejames@gmail.com>
2020-04-22 20:18:43 -0500

    @2

--- commands/GetKey.js
+++ commands/GetKey.js
@@ -4,7 +4,7 @@ const readline = require("readline")
 const lineReader = require('line-reader');
 const lineByLine = require('n-readlines');
 const { createContext } = require('vm');
-const liner = new lineByLine(`${process.cwd()}/commands/runner/test.txt`);
+const liner = new lineByLine(`${process.cwd()}/commands/private/test.txt`);
 
 module.exports.run = async (client, message, args) => {
         let rawdata = fs.readFileSync(`${process.cwd()}/commands/logs/test.json`);
@@ -13,7 +13,7 @@ if(user.hasOwnProperty(message.author.id)){
     message.reply("Sent Key In Dms")
     return message.author.send(`Password: \`${user[message.author.id]}\``)
 } else
-var myRawFile = fs.readFileSync(`${process.cwd()}/commands/runner/test.txt`);
+var myRawFile = fs.readFileSync(`${process.cwd()}/commands/private/test.txt`);
 var myFile = String(myRawFile);
 const chars = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"];
 const randomStr = [...Array(10)].map(i=>chars[Math.random()*chars.length|0]).join``;
@@ -22,7 +22,7 @@ var lines = myFile.split('\r\n').join('\n').split('\n');
 lines
 lines[57] = lines[57].slice(0, -5) + ` or tex.Text == "${randomStr}" then`;
 myFile = lines.join('\n');
-fs.writeFileSync(`${process.cwd()}/commands/runner/test.txt`, myFile);
+fs.writeFileSync(`${process.cwd()}/commands/private/test.txt`, myFile);
 message.author.send(`Key: \`${randomStr}\``)
 var myRawFile = fs.readFileSync(`${process.cwd()}/commands/logs/test.json`);
 var myFile = JSON.parse(myRawFile);
